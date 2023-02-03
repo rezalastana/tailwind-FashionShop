d1af01a0ebf5153b4745f4ab4c173eb64a38a689
Rezal Astana <rezalastana21@gmail.com>
2023-02-03 16:33:17 +0700

    Navbar custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
       fontFamily: {
         sans: ["Satoshi", "cursive"],
       },
+      colors: {
+        primary1: "#003E29",
+        primary2: "#E4BBAI",
+        primary3: "#D9D9D9",
+        borderColor: "#787878",
+        bgColor: "#EDEDED",
+        bgGreen: "#023423",
+      },
     },
   },
   plugins: [],
