efa78a711fa219bd9c582db51c68571a31499339
Rezal Astana <rezalastana21@gmail.com>
2023-02-03 23:23:21 +0700

    Hero grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       },
       colors: {
         primary1: "#003E29",
-        primary2: "#E4BBAI",
+        primary2: "#E4BBA1",
         primary3: "#D9D9D9",
         borderColor: "#787878",
         bgColor: "#EDEDED",
